c52bee933b449f0132ff1216ea3bad6c86e0a0fd
SattarovMK <sattarovmk@yandex.ru>
2024-12-12 00:06:14 +0500

    Add project files.

---
+++ FileProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelPractice
+{
+    public static class FileProcessor
+    {
+        public static async Task<KeyValuePair<string, int>[]> CountSpacesInDirectoryFiles(string directoryPath, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(directoryPath);
+            FileInfo[] files = dirInfo.GetFiles("*.*", searchOption);
+            Task[] tasks = new Task[files.Length];
+            KeyValuePair<string, int>[] result = new KeyValuePair<string, int>[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                var k = i;
+                var task = Task.Run(() =>
+                {
+                    result[k] = CountSpacesInFile(file.FullName);
+                });
+                tasks[i] = task;
+            }
+            await Task.WhenAll(tasks);
+            return result;
+        }
+
+        public static KeyValuePair<string, int> CountSpacesInFile(string filePath)
+        {
+            using StreamReader sr = new StreamReader(filePath);
+            int count = 0;
+            while (sr.Peek() != -1)
+            {
+                char symbol = (char)sr.Read();
+                if (char.IsWhiteSpace(symbol)) count++;
+            }
+            return new KeyValuePair<string, int>(filePath, count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ParallelPractice;
+using System.Diagnostics;
+var sw = Stopwatch.StartNew();
+var threeFiles = await FileProcessor.CountSpacesInDirectoryFiles("../../../Test");
+sw.Stop();
+foreach (var file in threeFiles)
+{
+    Console.WriteLine($"File: {file.Key}; Spaces: {file.Value}");
+}
+Console.WriteLine($"Elapsed time in ms: {sw.ElapsedMilliseconds}");
+
+sw.Restart();
+var buildFiles = await FileProcessor.CountSpacesInDirectoryFiles("./");
+sw.Stop();
+foreach (var file in buildFiles)
+{
+    Console.WriteLine($"File: {file.Key}; Spaces: {file.Value}");
+}
+Console.WriteLine($"Elapsed time in ms: {sw.ElapsedMilliseconds}");
